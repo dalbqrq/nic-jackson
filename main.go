bfc4a3f8a03839b6389bed0be8400c8c5e239047
Daniel Lins de Albuquerque <daniel@impa.br>
2023-03-19 11:17:59 -0300

    Episode 2

--- main.go
+++ main.go
@@ -1,32 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/dalbqrq/nic-jackson/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Go")
-		d, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
+	// Opcionalmente poderia registrar a funcao diretamente
+	// http.HandleFunc("/ok", hh.ServeHTTP)
+	// http.ListenAndServe(":9090", hh)
+
+	sm := http.NewServeMux()
+	sm.Handle("/hello", hh)
+	sm.Handle("/goodbye", gh)
+
+	// Podemos criar o servico diretamente ou configura-lo como abaixo
+	//http.ListenAndServe(":9090", sm)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(w, "ooops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
 
-		fmt.Fprintf(w, "Hello %s", d)
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	http.HandleFunc("/hello", func(http.ResponseWriter, *http.Request) {
-		log.Println("Hello Go")
-	})
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye Go")
-	})
-
-	http.ListenAndServe(":9090", nil)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
